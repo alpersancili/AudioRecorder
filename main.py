75a58601d17b7a5931a5f4c79b52487216d77f36
Alper Sancılı <alpersancili0@gmail.com>
2023-10-01 08:31:00 +0300

    audiorecorder added

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import sounddevice
+import tkinter
 from scipy.io.wavfile import write
-sr = 44100
-seconds = 5
-print("Recording\n")
-record_voice = sounddevice.rec(sr*seconds, samplerate=sr,channels=2)
-sounddevice.wait()
-write("mynewfile.wav",sr,record_voice)
-print("Finished")
+
+voice_file = "C:/Users/alper/OneDrive/Masaüstü/voice.wav"
+
+def record():
+    sr = 44100
+    seconds = 5
+    record_voice = sounddevice.rec(sr * seconds, samplerate=sr, channels=2)
+    sounddevice.wait()
+    write(voice_file, sr, record_voice)
+
+window = tkinter.Tk()
+window.title("VOICE RECORDER")
+window.minsize(width=400, height=400)
+
+btn = tkinter.Button(text="Record", command=record, bg="green")
+btn.config(height=3, width=10)
+btn.place(x=170, y=150)
+
+window.mainloop()
